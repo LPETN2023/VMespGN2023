ce191e0082770fd931fd30ccdec4b2ff56f52b10
LPETN2023 <151559483+LPETN2023@users.noreply.github.com>
2025-03-14 11:46:21 +0100

    Update telepathy

--- Fichiers_techniques/Installation_Apps_WSL.sh
+++ Fichiers_techniques/Installation_Apps_WSL.sh
@@ -8,8 +8,13 @@ sudo apt -y install whatweb
 sudo apt -y install python3-pip
 sudo pip3 install holehe
 sudo pip3 install ignorant
-sudo pip3 install telepathy
+cd $HOME
 git clone https://github.com/sherlock-project/sherlock.git
 cd sherlock
 sudo python3 -m pip install -r requirements.txt
-echo 'alias sherlock="python3 /home/ubuntu/sherlock/sherlock/sherlock.py"' >> ~/.bashrc
+echo 'alias sherlock="python3 $HOME/sherlock/sherlock/sherlock.py"' >> ~/.bashrc
+cd $HOME
+git clone https://github.com/LPETN2023/Telepathy_VM
+cd Telepathy_VM
+sudo python3 -m pip install -r requirements.txt
+echo 'alias telepathy="python3 $HOME/Telepathy_VM/Telegram_ESP.py"' >> ~/.bashrc
